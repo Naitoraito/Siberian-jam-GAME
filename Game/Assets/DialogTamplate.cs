75657ec7b4a617ef0bc6e4662773ef2d44905247
lastone-hikiray <test@mail.com>
2021-04-03 01:12:51 +0300

    Dialogue additions

--- Game/Assets/DialogTamplate.cs
+++ Game/Assets/DialogTamplate.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialogue", menuName = "DialogTamplate", order = 52)]
 public class DialogTamplate : ScriptableObject
 {
-    public Sprite character;
-    public Sprite opponent;
+    public GameObject addedItem;
+    public GameObject removedItem;
+    [Space(20)]
+    public Sprite characterSprite;
+    public Sprite opponentSprite;
+    [Space(20)]
     [TextArea(3, 10)]
     public string[] question;
-    public string Ybutton;
-    public string Nbutton;
-    [TextArea(3, 10)]
-    public string[] YAnswer;
-    [TextArea(3, 10)]
-    public string[] NAnswer;
+    public Answer Ybutton;
+    public Answer Nbutton;
+}
 
-
+[System.Serializable]
+public class Answer
+{
+    public string buttonText;
+    public int carma;
+    [TextArea(3, 10)]
+    public string[] answer;
 }
